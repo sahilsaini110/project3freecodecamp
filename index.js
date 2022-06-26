1da932ec485388344d6a9304c99dede0f1f928f8
Sahil <sohil170246@gmail.com>
2022-06-26 18:40:27 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const dotenv = require("dotenv");
+
+dotenv.config();
 var express = require("express");
 var mongo = require("mongodb");
 var mongoose = require("mongoose");
